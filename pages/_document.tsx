e4d505e6f0d5cd169f989a63fb6cd11adab479e1
Miguel Correia <miguelncorreia@gmail.com>
2021-02-06 19:50:23 +0100

    feat: add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,28 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#603cba" />
+          <meta name="theme-color" content="#ffffff" />
+
           {/* From https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_document.js */}
           <script
             async
