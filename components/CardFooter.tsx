8bd31baa5a6386ecb5781d8271177fb434b8f974
Miguel Correia <miguelncorreia@gmail.com>
2021-02-07 17:14:50 +0100

    feat: add card close button

--- components/CardFooter.tsx
+++ components/CardFooter.tsx
@@ -1,4 +1,7 @@
+import { mdiClose, mdiCrosshairs } from "@mdi/js";
+import Icon from "@mdi/react";
 import { format } from "date-fns";
+import Link from "next/link";
 import React from "react";
 import config from "../utils/config";
 
@@ -22,14 +25,23 @@ export const CardFooter: React.FC<CardFooterProps> = ({ ticker = "" }) => {
         </a>
         .
       </p>
-      <div className="absolute px-8 md:px-10 -top-3 md:-top-2 flex w-full justify-between z-10 text-indigo-300 text-xs">
-        <p>
-          {config.url}/{ticker}
-        </p>
-        <p>{formattedToday}</p>
+      <div className="absolute inset-0">
+        <div className="absolute px-8 md:px-10 -top-3 md:-top-2 flex w-full justify-between z-10 text-indigo-300 text-xs">
+          <p>
+            {config.url}/{ticker}
+          </p>
+          <p>{formattedToday}</p>
+        </div>
+        <div className="absolute -top-4 md:-top-3 right-2 m-0 text-indigo-100 cursor-pointer block z-10">
+          <Link href="/">
+            <a>
+              <Icon path={mdiClose} size="1.4rem" />
+            </a>
+          </Link>
+        </div>
+        <div className="absolute top-24 right-8 z-0 transform -translate-y-20 bg-indigo-700 rounded-full h-96 w-96 opacity-20"></div>
+        <div className="absolute top-0 z-0 transform scale-x-125 -translate-x-24 -translate-y-20 bg-indigo-700 rounded-full h-96 w-96 opacity-20"></div>
       </div>
-      <div className="absolute top-24 right-8 z-0 transform -translate-y-20 bg-indigo-700 rounded-full h-96 w-96 opacity-20"></div>
-      <div className="absolute top-0 z-0 transform scale-x-125 -translate-x-24 -translate-y-20 bg-indigo-700 rounded-full h-96 w-96 opacity-20"></div>
     </>
   );
 };
